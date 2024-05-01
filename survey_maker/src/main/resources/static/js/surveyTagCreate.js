4de84088a47fa479d6a28555f8e6fa6c2c788417
DaiKitajima <kitajima@w-bridge.co.jp>
2024-05-01 12:54:40 +0900

    指摘対応２

--- survey_maker/src/main/resources/static/js/surveyTagCreate.js
+++ survey_maker/src/main/resources/static/js/surveyTagCreate.js
@@ -5,6 +5,14 @@ $(function(){
 	$('#inputColorCode').val($("#surveyColor").val()); 
 	$('#shadowColorCode').val($("#surveyResultColor").val()); 
 	
+	// フォント字体コピー
+	$('#fontSizeTag').on('click', function() {
+		var content = "";
+		content = content + "<span style=\"font-size:12px;\">ここにテキストを入力</span>\n";
+		$("#surveyTagContent").val(content);
+		$("#surveyTagCreateDialog").modal("show");
+	});
+	
 	// 大見出しコピー
 	$('#mainTitleTag').on('click', function() {
 		var colorCD = $('#inputColorCode').val(); 
@@ -30,6 +38,7 @@ $(function(){
 	// 誘導ボタンコピー
 	$("#induceBtnTag").on('click', function() {
 		var content = "";
+		content = content + "<div style=\"width:100%;height:40px;\"></div>\n";
 		content = content + "<div class=\"row mt-2 mb-2\">\n";
 		content = content + "	<div class=\"col d-flex justify-content-center h6\">\n";
 		content = content + "		<a href=\"#\" class=\"btn btn-warning btn-lg text-light\" style=\"width:250px;border-radius:40px;\" ><span class=\"h5\">専門家に相談する　▶</span></a>\n";
